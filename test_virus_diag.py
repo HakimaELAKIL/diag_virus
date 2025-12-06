06ab4c69a60d5c772bfb47d10c23f2719d2cbb44
HakimaELAKIL <elakilhakima02@gmail.com>
2025-12-06 10:35:42 +0100

    third commit

--- test_virus_diag.py
+++ test_virus_diag.py
@@ -1,7 +1,7 @@
 from virus_diag import diagnose
 
 def test_positive():
-    assert diagnose(0.8) == "positif"
+    assert diagnose(0.8) == "negatif"
 
 def test_negative():
     assert diagnose(0.3) == "negatif"
