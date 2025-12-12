152adb6498781715aa2a49a3a8e40ed481bbf161
HakimaELAKIL <elakilhakima02@gmail.com>
2025-12-12 15:06:44 +0100

    update projet virus_diag lint correction

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,6 +4,7 @@ from core.model import VirusModel
 def test_virus_positive():
     model = VirusModel()
     assert model.diagnose([1, 0, 1]) == 1
+    
 
 def test_virus_negative():
     model = VirusModel()
